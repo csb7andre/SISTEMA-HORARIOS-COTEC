dadb6f0eb0f4f8c77ff058a4b550f56e5daf811c
csb7andre <csb7andre@gmail.com>
2026-02-08 06:30:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    title: "HOR4:R105 - COTEC",  // <--- AQUI O NOME COM DOIS PONTOS APARECE
+    // icon: path.join(__dirname, 'icon.png'), // Descomente se tiver icone
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
